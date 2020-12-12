6149216a2e9b2db3911647bb7469148946fbd502
Sadek Badredine <sadek@itxi.net>
2020-12-12 03:07:44 +0200

    created sidenav-list component

--- fitness-tracker/src/app/app.module.ts
+++ fitness-tracker/src/app/app.module.ts
@@ -6,15 +6,16 @@ import { MaterialModule } from './material.module';
 import { NgModule } from '@angular/core';
 
 import { CurrentTrainingComponent } from './pages/training/current-training/current-training.component';
+import { SidenavListComponent } from './components/navigation/sidenav-list/sidenav-list.component';
 import { PastTrainingComponent } from './pages/training/past-training/past-training.component';
+import { HeaderComponent } from './components/navigation/header/header.component';
 import { NewTrainingComponent } from './pages/training/new-training/new-training.component';
-import { SignupComponent } from './pages/auth/signup/signup.component';
 import { TrainingComponent } from './pages/training/training.component';
-import { LoginComponent } from './pages/auth/login/login.component';
+import { SignupComponent } from './pages/auth/signup/signup.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HeaderComponent } from './components/navigation/header/header.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { HeaderComponent } from './components/navigation/header/header.component
     PastTrainingComponent,
     WelcomeComponent,
     HeaderComponent,
+    SidenavListComponent,
   ],
   imports: [
     BrowserModule,
