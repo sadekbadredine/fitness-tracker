c0cc70ec42028f0c3be2dc502d2255d144178e77
Sadek Badredine <sadek@itxi.net>
2020-12-12 14:48:18 +0200

    folder structure re-organized

--- fitness-tracker/src/app/app.module.ts
+++ fitness-tracker/src/app/app.module.ts
@@ -5,11 +5,11 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from './material.module';
 import { NgModule } from '@angular/core';
 
-import { CurrentTrainingComponent } from './pages/training/current-training/current-training.component';
+import { CurrentTrainingComponent } from './components/training/current-training/current-training.component';
 import { SidenavListComponent } from './components/navigation/sidenav-list/sidenav-list.component';
-import { PastTrainingComponent } from './pages/training/past-training/past-training.component';
+import { PastTrainingComponent } from './components/training/past-training/past-training.component';
+import { NewTrainingComponent } from './components/training/new-training/new-training.component';
 import { HeaderComponent } from './components/navigation/header/header.component';
-import { NewTrainingComponent } from './pages/training/new-training/new-training.component';
 import { TrainingComponent } from './pages/training/training.component';
 import { SignupComponent } from './pages/auth/signup/signup.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
